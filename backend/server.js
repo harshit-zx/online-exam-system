ec2b783797945643f347271be7a6b4936bf1e5a7
Harshit Maurya <153452035+harshit-zx@users.noreply.github.com>
2026-03-15 22:14:36 +0530

    Update CORS middleware to support multiple origins

--- backend/server.js
+++ backend/server.js
@@ -15,12 +15,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // middleware
-app.use(
-  cors({
-    origin: "http://localhost:5173", 
-    credentials: true,
-  }),
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",                 // For local development
+    "https://exampro-yqyt.onrender.com"      // For live production
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 
 app.get("/", (req, res) => {
